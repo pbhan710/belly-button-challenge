f2cdc1c1dec5f1130e199a8501dde383f702c7c0
pbhan710 <pbhan710@gmail.com>
2022-12-22 14:27:56 -0500

    Added code to add bubble chart.

--- static/js/app.js
+++ static/js/app.js
@@ -15,9 +15,18 @@ function init() {
         let names = result.names;
         let samples = result.samples;
 
-        console.log(result)
         // Plot top 10 OTUs horizontal bar chart from first test subject ID.
         plotTop10OTUs(0);
+        // Plot bubble chart that displays each sample.
+        plotSamplesBubbleChart(0);
+    });
+};
+
+
+function populateDemographics(index) {
+    promiseObject.then(function(result) {
+        let sample = result.samples[index];
+
     })
 }
 
@@ -25,7 +34,7 @@ function init() {
 function plotTop10OTUs (index) {
     // Fetch JSON results from the Promise object.
     promiseObject.then(function(result) {
-        // Store the index's sample array and its keys for plotting in variables.
+        // Store the index's sample array and its keys for plotting in variables, 10 top only.
         let sample = result.samples[index];
         let otuIDs = sample.otu_ids.slice(0, 10).map(item => `OTU ${item}`);
         let values = sample.sample_values.slice(0, 10);
@@ -46,14 +55,45 @@ function plotTop10OTUs (index) {
         }];
 
         let layout = {
-            title: `Top 10 OTUs for Test Subject ID ${sample.id}`
+            title: `Top 10 OTUs for Test Subject ID ${sample.id}`   // Add title to chart.
         };
 
-        console.log(sample);
         // Plot the horizontal bar chart.
         Plotly.newPlot("bar", data, layout);
-    })
-}
+    });
+};
+
+// Define a function to plot each sample in a bubble chart.
+function plotSamplesBubbleChart (index) {
+    promiseObject.then(function(result) {
+        // Store the index's sample array and its keys for plotting in variables.
+        let sample = result.samples[index];
+        let otuIDs = sample.otu_ids;
+        let values = sample.sample_values;
+        let otuLabels = sample.otu_labels;
+
+        // Store Plotly objects for plotting.
+        let data = [{
+            x: otuIDs,
+            y: values,
+            text: otuLabels,
+            mode: "markers",
+            marker: {
+                color: otuIDs,
+                colorscale: "Earth",
+                size: values,
+                sizeref: 1.5
+            }
+        }];
+
+        let layout = {
+            title: `Test Subject ID ${sample.id}'s Samples`   // Add title to chart.
+        };
+
+        // Plot the bubble chart.
+        Plotly.newPlot("bubble", data, layout);
+    });
+};
 
 // Define a function to populate the test subject ID dropdown.
 function populateDropdown() {
@@ -65,11 +105,11 @@ function populateDropdown() {
         // Loop through names array and append each ID to the dropdown.
         for (i = 0; i < names.length; i++) {
             // Select the dropdown and append an option tag with the ID.
-            let dropdownOption = d3.select("#selDataset").append("option")
+            let dropdownOption = d3.select("#selDataset").append("option");
             // Assign the ID itself as the text to the appended option tag.
-            dropdownOption.text(names[i])
+            dropdownOption.text(names[i]);
         }
-    })
-}
+    });
+};
 
-init()
+init();
