667ebc037a2306aa991a95243df956ee2bca0f92
pbhan710 <pbhan710@gmail.com>
2022-12-21 21:30:59 -0500

    Added code to populate dropdown upon HTML initializing.

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,30 @@
+// Store in constant the URL of samples.json.
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-const promiseSamples = d3.json(url);
+// Obtain and store in constant a Promise object from the samples.json URL.
+const promiseObject = d3.json(url);
 
-promiseSamples.then(function(data) {
-    console.log(data);
-})
+// Define an initiate function.
+function init() {
+    // 
+    populateDropdown();
+}
+
+// Define a function to populate the test subject ID dropdown.
+function populateDropdown() {
+    // Fetch JSON results from the Promise.
+    promiseObject.then(function(result) {
+        // Store names array in variable.
+        let names = result.names;
+
+        // Loop through names array and append each ID to the dropdown.
+        for (i = 0; i < names.length; i++) {
+            // Select the dropdown and append an option tag with the ID.
+            let dropdownOption = d3.select("#selDataset").append("option")
+            // Assign the ID itself as the text to the appended option tag.
+            dropdownOption.text(names[i])
+        }
+    })
+}
+
+init()
