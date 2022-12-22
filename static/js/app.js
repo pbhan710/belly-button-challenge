2609a1ad66cc0b2e675d2883e6259a623349d58c
pbhan710 <pbhan710@gmail.com>
2022-12-22 09:45:56 -0500

    Added draft code to plot the top 10 OTUs horizontal bar chart.

--- static/js/app.js
+++ static/js/app.js
@@ -6,15 +6,48 @@ const promiseObject = d3.json(url);
 
 // Define an initiate function.
 function init() {
-    // 
+    // Populate the test subject ID dropdown
     populateDropdown();
+
+    // Fetch JSON results from the Promise object.
+    promiseObject.then(function(result) {
+        // Store arrays in variables.
+        let names = result.names;
+        let samples = result.samples;
+
+        // Plot top 10 OTUs horizontal bar chart from first test subject ID.
+        plotTop10OTUs(0);
+    })
+}
+
+// Define a function to plot the top 10 OTUs horizontal bar chart.
+function plotTop10OTUs (index) {
+    // Fetch JSON results from the Promise object.
+    promiseObject.then(function(result) {
+        // Store the index's sample array and its keys for plotting in variables.
+        let sample = result.samples[index];
+        let otuIDs = sample.otu_ids;
+        let values = sample.sample_values;
+        let otuLabels = sample.otu_labels;
+
+        // Store Plotly objects for plotting.
+        let data = [{
+            type: "bar",
+            x: sample.sample_values,
+            y: sample.otu_ids,
+            orientation: "h"
+        }];
+
+        // Plot the horizontal bar chart.
+        Plotly.newPlot("bar", data);
+    })
 }
 
 // Define a function to populate the test subject ID dropdown.
 function populateDropdown() {
-    // Fetch JSON results from the Promise.
+    // Fetch JSON results from the Promise object.
     promiseObject.then(function(result) {
-        // Store names array in variable.
+        // Store names array in a variable.
         let names = result.names;
 
         // Loop through names array and append each ID to the dropdown.
